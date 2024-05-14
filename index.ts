2ba9094bae4645f6ecaf8d9337a5fad524948f14
kimbapquyenduy <lequyenduy.tdc2020@gmail.com>
2024-05-14 14:56:12 +0700

    Add teacher protobuff

--- index.ts
+++ index.ts
@@ -2,3 +2,4 @@ export * from './common/entities/order';
 export * from './common/entities/product';
 export * from './common/entities/employee';
 export * from './common/entities/film';
+export * from './common/entities/teacher';
